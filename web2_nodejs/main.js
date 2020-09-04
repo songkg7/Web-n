c8b6e56cff67fe7b33fa41cec36bded1a0a98413
songkg7 <56438906+songkg7@users.noreply.github.com>
2020-09-05 06:58:31 +0900

    Refactoring

--- web2_nodejs/main.js
+++ web2_nodejs/main.js
@@ -2,38 +2,72 @@ var http = require("http");
 var fs = require("fs");
 var url = require("url");
 
-var app = http.createServer(function (request, response) {
+function templateHTML(title, list, body) {
+  return `<!DOCTYPE html>
+          <html>
+          <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8" />
+          </head>
+          <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              ${body}
+          </body>
+          </html>`;
+}
+
+function templateList(filelist) {
+  var list = "<ul>";
+  var i = 0;
+  while (i < filelist.length) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i++;
+  }
+  list = list + "</ul>";
+  return list;
+}
+
+var app = http.createServer((request, response) => {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
-  var title = queryData.id;
-  if (_url == "/") {
-    title = "Welcome";
+  var pathname = url.parse(_url, true).pathname;
+
+  if (pathname === "/") {
+    if (queryData.id === undefined) {
+      fs.readdir("./data", (error, filelist) => {
+        var title = "Welcome";
+        var description = "Hello, Node.js";
+        var list = templateList(filelist);
+        var template = templateHTML(
+          title,
+          list,
+          `<h2>${title}</h2>${description}`
+        );
+
+        response.writeHead(200); // 정상적으로 서버 통신이 완료되었을때 받는 값 = 200
+        response.end(template);
+      });
+    } else {
+      fs.readdir("./data", (error, filelist) => {
+        fs.readFile(`data/${queryData.id}`, "utf8", (err, description) => {
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(
+            title,
+            list,
+            `<h2>${title}</h2>${description}`
+          );
+
+          response.writeHead(200); // 정상적으로 서버 통신이 완료되었을때 받는 값 = 200
+          response.end(template);
+        });
+      });
+    }
+  } else {
+    response.writeHead(404); // 파일을 찾을 수 없을 때 서버가 돌려주는 번호 404
+    response.end("Not found");
   }
-  if (_url == "/favicon.ico") {
-    return response.writeHead(404);
-  }
-  response.writeHead(200);
-  fs.readFile(`data/${title}`, "utf8", (err, description) => {
-    var template = `
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8" />
-        </head>
-        <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>${description}</p>
-        </body>
-        </html>
-      `;
-    response.end(template);
-  });
 });
+
 app.listen(3000); // 80이 웹의 기본값
