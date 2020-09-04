0f137f03d20ad41298169ff8b8bfb0ae3d21670f
songkg7 <56438906+songkg7@users.noreply.github.com>
2020-09-04 17:57:21 +0900

    Add fileread

---
+++ web2_nodejs/main.js
@@ -0,0 +1,39 @@
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var title = queryData.id;
+  if (_url == "/") {
+    title = "Welcome";
+  }
+  if (_url == "/favicon.ico") {
+    return response.writeHead(404);
+  }
+  response.writeHead(200);
+  fs.readFile(`data/${title}`, "utf8", (err, description) => {
+    var template = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8" />
+        </head>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}</p>
+        </body>
+        </html>
+      `;
+    response.end(template);
+  });
+});
+app.listen(3000); // 80이 웹의 기본값
