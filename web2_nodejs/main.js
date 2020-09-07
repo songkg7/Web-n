652896e4ada6e1551866802d28efb0dc6ae6d9a6
songkg7 <56438906+songkg7@users.noreply.github.com>
2020-09-08 05:20:52 +0900

    Add post write

--- web2_nodejs/main.js
+++ web2_nodejs/main.js
@@ -1,8 +1,9 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
+var qs = require("querystring");
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control) {
   return `<!DOCTYPE html>
           <html>
           <head>
@@ -12,6 +13,7 @@ function templateHTML(title, list, body) {
           <body>
               <h1><a href="/">WEB</a></h1>
               ${list}
+              ${control}
               ${body}
           </body>
           </html>`;
@@ -42,7 +44,8 @@ var app = http.createServer((request, response) => {
         var template = templateHTML(
           title,
           list,
-          `<h2>${title}</h2>${description}`
+          `<h2>${title}</h2>${description}`,
+          `<a href="/create">create</a>`
         );
 
         response.writeHead(200); // 정상적으로 서버 통신이 완료되었을때 받는 값 = 200
@@ -56,7 +59,8 @@ var app = http.createServer((request, response) => {
           var template = templateHTML(
             title,
             list,
-            `<h2>${title}</h2>${description}`
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
           );
 
           response.writeHead(200); // 정상적으로 서버 통신이 완료되었을때 받는 값 = 200
@@ -64,6 +68,91 @@ var app = http.createServer((request, response) => {
         });
       });
     }
+  } else if (pathname === "/create") {
+    fs.readdir("./data", (error, filelist) => {
+      var title = "WEB - create";
+      var list = templateList(filelist);
+      var template = templateHTML(
+        title,
+        list,
+        `<form action="/create_process" method="post">
+          <p>
+            <input type="text" name="title" placeholder="title" />
+          </p>
+          <p>
+            <textarea name="description" placeholder ="description" id="" cols="30" rows="10"></textarea>
+          </p>
+          <p>
+            <input type="submit" />
+          </p>
+        </form>`,
+        ""
+      );
+
+      response.writeHead(200); // 정상적으로 서버 통신이 완료되었을때 받는 값 = 200
+      response.end(template);
+    });
+  } else if (pathname === "/create_process") {
+    var body = "";
+    request.on("data", function (data) {
+      body = body + data;
+    });
+    request.on("end", function () {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`data/${title}`, description, "utf8", (err) => {
+        response.writeHead(302, { Location: `/?id=${title}` });
+        response.end(template);
+      });
+    });
+  } else if (pathname === "/update") {
+    fs.readdir("./data", (error, filelist) => {
+      fs.readFile(`data/${queryData.id}`, "utf8", (err, description) => {
+        var title = queryData.id;
+        var list = templateList(filelist);
+        var template = templateHTML(
+          title,
+          list,
+          `
+          <form action="/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+          <p>
+            <input type="text" name="title" placeholder="title" value="${title}"/>
+          </p>
+          <p>
+            <textarea name="description" placeholder ="description" id="" cols="30" rows="10">${description}</textarea>
+          </p>
+          <p>
+            <input type="submit" />
+          </p>
+        </form>
+        `,
+          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+        );
+
+        response.writeHead(200); // 정상적으로 서버 통신이 완료되었을때 받는 값 = 200
+        response.end(template);
+      });
+    });
+  } else if (pathname === "/update_process") {
+    var body = "";
+    request.on("data", function (data) {
+      body = body + data;
+    });
+    request.on("end", function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`, (error) => {
+        fs.writeFile(`data/${title}`, description, "utf8", (err) => {
+          response.writeHead(302, { Location: `/?id=${title}` });
+          response.end();
+        });
+      });
+      console.log(post);
+    });
   } else {
     response.writeHead(404); // 파일을 찾을 수 없을 때 서버가 돌려주는 번호 404
     response.end("Not found");
