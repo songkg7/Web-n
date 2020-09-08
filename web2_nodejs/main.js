274d1627294598f886badd044a4584420797f6ca
songkg7 <56438906+songkg7@users.noreply.github.com>
2020-09-09 07:21:59 +0900

    Error update

--- web2_nodejs/main.js
+++ web2_nodejs/main.js
@@ -87,7 +87,7 @@ var app = http.createServer((request, response) => {
       var description = post.description;
       fs.writeFile(`data/${title}`, description, "utf8", (err) => {
         response.writeHead(302, { Location: `/?id=${title}` });
-        response.end(template);
+        response.end();
       });
     });
   } else if (pathname === "/update") {
